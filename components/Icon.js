b4cc84eb4c142215608a58f627325e95b1ea41c3
Thomas Deconinck <thomas.d@colisweb.com>
2017-02-15 11:44:44 +0100

    Add Icon component

---
+++ components/Icon.js
@@ -0,0 +1,25 @@
+// @flow
+
+/* Check http://fontawesome.io/icons/ for the list */
+
+import React from 'react'
+import classNames from 'classnames'
+
+type Props = {
+  name: string,
+  size: 'small' | 'medium' | 'large'
+}
+
+export default (props: Props): React$Element<*> => {
+  const className = classNames('icon', {
+    'is-small': props.size === 'small',
+    'is-medium': props.size === 'medium',
+    'is-large': props.size === 'large'
+  })
+
+  return (
+    <span className='icon'>
+      <i className={`fa fa-${props.name}`} />
+    </span>
+  )
+}
