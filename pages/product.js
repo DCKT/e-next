ed1cf4a60e25b4c3d64ceaa794b36332d8e3fb7a
Thomas Deconinck <thomas.d@colisweb.com>
2017-02-15 09:23:32 +0100

    Improve detail page

--- pages/product.js
+++ pages/product.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Layout from '../components/Layout'
 import * as Moltin from '../utils/js/moltin'
 import ProductListItem from '../components/ProductListItem'
+import Button from '../components/Button'
 import type { TMoltinProduct } from '../utils/js/types'
 
 export default class Home extends React.Component {
@@ -15,21 +16,33 @@ export default class Home extends React.Component {
 
   render () {
     const { product } = this.props
-    const { title, description, brand } = product
+    const { title, description, brand, images } = product
 
     return (
       <Layout title={title}>
-        <section className='section'>
-          <div className='container'>
-            <div className='heading'>
-              <h1 className='title'>{ title }</h1>
-              <h2 className='subtitle'>{ brand.value }</h2>
+        <div className='container'>
+          <div className='columns'>
+            <div className='column is-half'>
+              <img src={images[0].url.http} alt={images[0].name} />
             </div>
-            <p className='content'>
-              { description }
-            </p>
+            <div className='column is-half'>
+              <section className='section'>
+                <div className='heading'>
+                  <h1 className='title'>{ title }</h1>
+                  <h2 className='subtitle'>{ brand.value }</h2>
+                </div>
+                <p className='content'>
+                  { description }
+                </p>
+                <div>
+                  <Button type='primary'>
+                    Ajouter au panier
+                  </Button>
+                </div>
+              </section>
+            </div>
           </div>
-        </section>
+        </div>
       </Layout>
     )
   }
