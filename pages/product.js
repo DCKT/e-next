8cd389851738d99e1556322e501f9fc73dd8c2fc
Thomas Deconinck <thomas.d@colisweb.com>
2017-02-15 11:45:15 +0100

    Add basic gallery system on product detail

--- pages/product.js
+++ pages/product.js
@@ -4,9 +4,15 @@ import Layout from '../components/Layout'
 import * as Moltin from '../utils/js/moltin'
 import ProductListItem from '../components/ProductListItem'
 import Button from '../components/Button'
-import type { TMoltinProduct } from '../utils/js/types'
+import type { TMoltinProduct, TMoltinImage } from '../utils/js/types'
+
+type State = {
+  currentPicture: TMoltinImage
+}
 
 export default class Home extends React.Component {
+  state: State
+
   static async getInitialProps ({ query }) {
     const id = query.id ? query.id : query.slug.split('_')[1]
     const product = await Moltin.fetchProduct(id)
@@ -14,16 +20,38 @@ export default class Home extends React.Component {
     return { product }
   }
 
+  componentDidMount () {
+    this.setState({ currentPicture: this.props.product.images[0] })
+  }
+
+  constructor () {
+    super()
+
+    this.state = {
+      currentPicture: null
+    }
+  }
+
   render () {
     const { product } = this.props
+    const { currentPicture } = this.state
     const { title, description, brand, images } = product
 
-    return (
+    return product ? (
       <Layout title={title}>
         <div className='container'>
           <div className='columns'>
             <div className='column is-half'>
-              <img src={images[0].url.http} alt={images[0].name} />
+              <div>
+                {
+                  currentPicture ? <img src={currentPicture.url.http} alt={currentPicture.name} /> : null
+                }
+              </div>
+              <div className='columns'>
+                {
+                  images.map(this._renderPictures)
+                }
+              </div>
             </div>
             <div className='column is-half'>
               <section className='section'>
@@ -44,6 +72,15 @@ export default class Home extends React.Component {
           </div>
         </div>
       </Layout>
-    )
+    ) : null
+  }
+
+  _renderPictures = (picture: TMoltinImage, i: number): React$Element<*> =>
+    <div className='column'>
+      <img src={picture.url.http} alt={picture.name} onClick={this._changeCurrentPicture(picture)} />
+    </div>
+
+  _changeCurrentPicture = (picture: TMoltinImage): Function => () => {
+    this.setState({ currentPicture: picture })
   }
 }
