6bc219d3a4367e695d585a8135834681b715a567
Thomas Deconinck <thomas.d@colisweb.com>
2017-02-17 16:24:53 +0100

    fix authenticate moltin

--- utils/js/moltin.js
+++ utils/js/moltin.js
@@ -5,18 +5,19 @@ const moltin = require('moltin')({
   secretKey: process.env.ENEXT_MOLTIN_PRIVATE
 })
 
-const sdk = moltin.Authenticate(() => {
-  console.info('Moltin authenticated !')
-})
-
+const Authenticate = cb => moltin.Authenticate(() => cb())
 
 export const fetchProducts = (): Promise<Array<TMoltinProduct>> =>
   new Promise((resolve, reject) => {
-    sdk.Product.List(null, data => resolve(data), error => reject(error))
+    Authenticate(() => {
+      moltin.Product.List(null, data => resolve(data), error => reject(error))
+    })
   })
 
 export const fetchProduct = (id: string): Promise<TMoltinProduct> =>
   new Promise((resolve, reject) => {
-    sdk.Product.Get(id, data => resolve(data), error => reject(error))
+    Authenticate(() => {
+      moltin.Product.Get(id, data => resolve(data), error => reject(error))
+    })
   })
 
